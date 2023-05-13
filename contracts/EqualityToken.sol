1cfd919b571b94a9afa1dfaf4ddf6d5c78347291
CatalinBalut <balut.catalin@yahoo.com>
2023-05-13 22:08:19 +0300

    boilerplate + first version of token

---
+++ contracts/EqualityToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+//import OZ ERC20
+
+contract EqualityToken {
+
+    function claim() public {
+        //verification - worldcoin, polyID, POH 
+        //verifyAt
+
+        //mint 1 token ERC20
+    }
+
+    function execute(address scontract, string calldata selector) public {
+        //verify if contract is IQuality
+
+        //low level to scontract
+    }
+
+}
